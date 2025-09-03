6dbe0357d80430eb101d48962407593be8308674
Eshan Singh <progressivesingh@gmail.com>
2025-09-04 00:27:49 +0530

    patch up

--- coremfup/mfup/cli.py
+++ coremfup/mfup/cli.py
@@ -1,6 +1,6 @@
 import argparse
 from InquirerPy import inquirer
-from mfup.downloader import download_audio, download_video, download_video_only, get_video_formats
+from mfup.downloader import download_audio, download_video, get_video_formats
 
 def main():
     parser = argparse.ArgumentParser(description="mfup - Media Fire Up")
@@ -8,25 +8,21 @@ def main():
     parser.add_argument("--debug", action="store_true", help="Show detailed yt-dlp logs")
     args = parser.parse_args()
 
-
     debug_mode = args.debug
+
 
     choice = inquirer.select(
         message="What do you want to download?",
-        choices=["Audio only", "Video (with audio)", "Video only (no audio)"],
+        choices=["Audio only", "Video (with audio)"],
     ).execute()
 
-    if choice in ["Video (with audio)", "Video only (no audio)"]:
+    if choice == "Video (with audio)":
         formats = get_video_formats(args.url, debug=debug_mode)
         quality = inquirer.select(
             message="Choose video quality:",
             choices=formats,
         ).execute()
-
-        if choice == "Video (with audio)":
-            download_video(args.url, quality, debug=debug_mode)
-        else:
-            download_video_only(args.url, quality, debug=debug_mode)
+        download_video(args.url, quality, debug=debug_mode)
 
     elif choice == "Audio only":
         audio_format = inquirer.select(
